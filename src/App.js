c8de1b8b47715a581afb18cda2af067fa8de041e
Wojciech Kleszcz <wkleszcz@student.agh.edu.pl>
2025-01-12 15:46:50 +0100

    finished interactive movie adding

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import "milligram";
 import {useState} from "react";
 
 function App() {
-  const [title, setTitle] = useState('Wall-E');
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [movies, setMovies] = useState([]);
 
-  const movies = [
-    {title: "Wall-E"},
-    {title: "Pulp Fiction"},
-    {title: "Matrix"},
-    {title: "1670"},
-  ];
-
-  function handleChange(event) {
+  function handleTitleChange(event) {
     setTitle(event.target.value);
+  }
+
+  function handleYearChange(event) {
+    setYear(event.target.value);
+  }
+
+  function handleMovieSubmit(event)
+  {
+    const newMovie = {};
+    newMovie.title = title
+    newMovie.year = year
+    setMovies([...movies, newMovie]);
   }
 
   let message = "";
@@ -30,15 +37,30 @@ function App() {
 
       <h2>Titles</h2>
       <ul>
-        {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+        {movies.map((movie) => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
       </ul>
 
-      <h2>My favourite movie for today is {title}</h2>
+      <h2>Add movie</h2>
+
+      <h3>Title</h3>
+      <input type="text" value={title} onChange={handleTitleChange}/>
+
+      {
+        title.length > 0 && <div>{message}</div>
+      }
+
+      <h3>Year</h3>
+      <input type="text" value={year} onChange={handleYearChange}/>
+
+      <button onClick={handleMovieSubmit}>Add</button>
+
+
+      {/* <h2>My favourite movie for today is {title}</h2>
       {
         title.length > 0 && <div>{message}</div>
       }
       <input type="text" value={title} onChange={handleChange}/>
-      <button onClick={() => alert(title)}>Pokaż tytuł filmu</button>
+      <button onClick={() => alert(title)}>Pokaż tytuł filmu</button> */}
     </div>
   );
 }
