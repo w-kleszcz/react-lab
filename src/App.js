193a1cfdd0644fc80e95435493b5a9d7c0ea11e8
Wojciech Kleszcz <wkleszcz@student.agh.edu.pl>
2025-01-12 14:59:28 +0100

    added list of titles and milligram css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import "milligram";
 import {useState} from "react";
 
 function App() {
   const [title, setTitle] = useState('Wall-E');
 
+  const movies = [
+    {title: "Wall-E"},
+    {title: "Pulp Fiction"},
+    {title: "Matrix"},
+    {title: "1670"},
+  ];
+
   function handleChange(event) {
-      setTitle(event.target.value);
+    setTitle(event.target.value);
   }
 
   let message = "";
@@ -19,11 +27,18 @@ function App() {
   return (
     <div>
       <h1>My favourite movies to watch</h1>
+
+      <h2>Titles</h2>
+      <ul>
+        {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+      </ul>
+
       <h2>My favourite movie for today is {title}</h2>
       {
         title.length > 0 && <div>{message}</div>
       }
       <input type="text" value={title} onChange={handleChange}/>
+      <button onClick={() => alert(title)}>Pokaż tytuł filmu</button>
     </div>
   );
 }
